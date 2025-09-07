ea5e5a4601c89f6621752b8aa27cf1810a5bf521
Mohammad Amzad <hossainamzad38@gmail.com>
2025-09-07 12:25:54 +0600

    Added font and Button

--- app/components/FirstLayout.jsx
+++ app/components/FirstLayout.jsx
@@ -16,65 +16,72 @@ export default function ThreeColumnNews() {
 
   return (
     <section>
-      <div className="flex flex-row w-12/12 gap-10 my-10">
-        {/* Left column */}
-        <div className="w-[25%]">
-          {leftItems.slice(0, 4).map((news, idx) => (
-            <Link
-              href={news.slug}
-              key={news.slug ?? `left-${idx}`}
-              className="block group border-b-1 border-gray-300"
-            >
-              <p className="font-bold text-2xl pb-4 group-hover:text-red-500 mt-10">
-                {news.headline}
-              </p>
-              <p className="text-gray-600 line-clamp-3 mb-10">
-                {news.newsArticle}
-              </p>
-            </Link>
-          ))}
-        </div>
+      <div className="place-items-center">
+        <div className="flex flex-row w-12/12 gap-10 mt-10">
+          {/* Left column */}
+          <div className="w-[25%]">
+            {leftItems.slice(0, 4).map((news, idx) => (
+              <Link
+                href={news.slug}
+                key={news.slug ?? `left-${idx}`}
+                className="block group border-b-1 border-gray-300"
+              >
+                <p className="font-bold text-2xl pb-4 group-hover:text-red-500 mt-10">
+                  {news.headline}
+                </p>
+                <p className="text-gray-600 line-clamp-3 mb-10">
+                  {news.newsArticle}
+                </p>
+              </Link>
+            ))}
+          </div>
 
-        {/* Middle column — only one item (index 1) */}
-        <div className="w-[50%] my-15">
-          {middleItem ? (
-            <Link href={middleItem.slug} className="group text-center">
-              <div className="relative h-[400px] w-full ">
-                <Image
-                  src={middleItem?.image}
-                  fill
-                  alt={middleItem.headline}
-                  priority
-                />
-              </div>
-              <p className="font-extrabold leading-[1.5] text-4xl my-5 group-hover:text-red-500">
-                {middleItem.headline}
-              </p>
-              <p className="text-gray-700 line-clamp-3">
-                {middleItem.newsArticle}
-              </p>
-            </Link>
-          ) : (
-            <div className="text-gray-500">No middle item</div>
-          )}
-        </div>
+          {/* Middle column — only one item (index 1) */}
+          <div className="w-[50%] my-15">
+            {middleItem ? (
+              <Link href={middleItem.slug} className="group text-center">
+                <div className="relative h-[400px] w-full ">
+                  <Image
+                    src={middleItem?.image}
+                    fill
+                    alt={middleItem.headline}
+                    priority
+                  />
+                </div>
+                <p className="font-extrabold leading-[1.5] text-4xl my-5 group-hover:text-red-500">
+                  {middleItem.headline}
+                </p>
+                <p className="text-gray-700 line-clamp-3">
+                  {middleItem.newsArticle}
+                </p>
+              </Link>
+            ) : (
+              <div className="text-gray-500">No middle item</div>
+            )}
+          </div>
 
-        {/* Right column */}
-        <div className="w-[25%]">
-          {rightItems.slice(5, 9).map((news, idx) => (
-            <Link
-              href={news.slug}
-              key={news.slug ?? `right-${idx}`}
-              className="block group border-b-1 border-gray-300"
-            >
-              <p className="font-bold text-2xl group-hover:text-red-500 mt-10 pb-4">
-                {news.headline}
-              </p>
-              <p className="text-gray-600 line-clamp-3 mb-10">
-                {news.newsArticle}
-              </p>
-            </Link>
-          ))}
+          {/* Right column */}
+          <div className="w-[25%]">
+            {rightItems.slice(5, 9).map((news, idx) => (
+              <Link
+                href={news.slug}
+                key={news.slug ?? `right-${idx}`}
+                className="block group border-b-1 border-gray-300"
+              >
+                <p className="font-bold text-2xl group-hover:text-red-500 mt-10 pb-4">
+                  {news.headline}
+                </p>
+                <p className="text-gray-600 line-clamp-3 mb-10">
+                  {news.newsArticle}
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <a href="#" className="text-center mb-10 rounded-lg px-6 py-2 font-bold text-lg text-white bg-[#303C5D]">
+            সব খবর
+          </a>
         </div>
       </div>
     </section>
--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ import Firstlayout from "./components/FirstLayout";
 export default function Home() {
   return (
     <main>
-      <section className="max-w-7xl mx-auto">
-        <div className=" "><Firstlayout /></div>
+      <section className="max-w-7xl mx-auto place-items-center">
+        <div ><Firstlayout /></div>
+        
       </section>
     </main>
   );
